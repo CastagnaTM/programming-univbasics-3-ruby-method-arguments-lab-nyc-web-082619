f2a6d8f5ff722790a3e0c97b848e07ef6e50af31
Thomas Castagna <castagna.tm@gmail.com>
2019-07-24 14:44:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,8 +4,11 @@ end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+<<<<<<< HEAD
 end
 
 def introduction_with_language_optional (name, language = "Ruby")
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+>>>>>>> 331e8973517abc334bfd3c935c3446f94be8f858
 end
